3cbb1247a5b290728b158d0850413fcc0eae75a6
Sougata Ghosh <sougataghosh314@gmail.com>
2025-04-14 00:30:26 +0530

    added JWT + NKEY + TLS authentication and set up couple of clients: user-service and api-gateway

--- api-gateway/src/main/java/com/sougata/nats/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/sougata/nats/ApiGatewayApplication.java
@@ -3,7 +3,10 @@ package com.sougata.nats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.sougata.nats",
+        "com.sougata.natscore"
+})
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
--- nats-core-components/src/main/java/com/sougata/natscore/config/ConnectionConfig.java
+++ nats-core-components/src/main/java/com/sougata/natscore/config/ConnectionConfig.java
@@ -1,14 +1,18 @@
 package com.sougata.natscore.config;
 
+import com.sougata.natscore.util.NatsSslUtils;
 import io.nats.client.Connection;
 import io.nats.client.Nats;
+import io.nats.client.Options;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 @Configuration
 @ConfigurationProperties(prefix = "nats")
@@ -16,9 +20,21 @@ import java.io.IOException;
 @Setter
 public class ConnectionConfig {
     private String url;
+    private String credsFile;
 
     @Bean(destroyMethod = "close")
-    public Connection connection() throws IOException, InterruptedException {
-        return Nats.connect(url);
+    public Connection connection() throws Exception {
+        Options options = new Options.Builder()
+                .server(url) // Or whatever secure URL your server runs on
+                .authHandler(Nats.credentials(credsFile))
+                .sslContext(NatsSslUtils.createSslContext(
+                        "C:/nats-config/certs/ca.pem",
+                        null, // No client cert needed for one-way TLS
+                        null  // No client key needed for one-way TLS
+                ))
+                .connectionTimeout(Duration.ofSeconds(5))
+                .build();
+
+        return Nats.connect(options);
     }
 }
